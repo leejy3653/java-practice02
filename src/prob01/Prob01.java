34053deea6a05900306afad962bb361df0a80ffb
bit <bit@DESKTOP-1LOO2I6>
2019-08-07 19:17:35 +0900

    완료

--- src/prob01/Prob01.java
+++ src/prob01/Prob01.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class Prob01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in  );
-		
+		Scanner scanner = new Scanner(System.in);
 		System.out.print( "금액: " );
 		int money = scanner.nextInt();
-		
-		scanner.close();
+		int[] coinUnit = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+		System.out.println("금액 : " + money);
+		for (int i = 0; i < coinUnit.length; i++) {
+			System.out.println(coinUnit[i]+"원 : "+ money / coinUnit[i]+"개");
+			money%= coinUnit[i];
+			scanner.close();
  	}
+}
 }
--- src/prob02/Prob02.java
+++ src/prob02/Prob02.java
@@ -3,18 +3,19 @@ package prob02;
 import java.util.Scanner;
 
 public class Prob02 {
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
-		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
+		int intArray[] = new int [5];
+		double sum = 0;
 		System.out.println( "5개의 숫자를 입력하세요." );
-		
-		/* 배열에 저장된 정수 값 더하기 */
-		
-		/* 출력 */
-		
-		/* 자원정리 */
-		scanner.close();
+	      for(int i=0; i<intArray.length; i++) {
+	          intArray[i] = scanner.nextInt();
+	       }		
+	     for(int i=0;i<intArray.length;i++)
+	        {
+	            sum += intArray[i];
+	        }
+	        System.out.print("평균은 " 
+	                       + sum/intArray.length + "입니다.");
+	    }
 	}
-}
